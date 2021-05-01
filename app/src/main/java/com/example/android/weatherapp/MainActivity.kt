fedaa2a87f1a7938b98cdf3f36129fc3e162a946
Prince Pandey <princekupandey786@gmail.com>
2021-05-01 21:17:23 +0530

    Added Dexter

--- app/src/main/java/com/example/android/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/android/weatherapp/MainActivity.kt
@@ -1,11 +1,153 @@
 package com.example.android.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.*
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.graphics.Typeface
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat.*
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToast.Companion.darkToast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+           if(!isLocationEnabled()){
+               darkToast(this,"ERROR","Your location is turned Off. Please turned it ON.",
+                       MotionToast.TOAST_ERROR,
+                       MotionToast.GRAVITY_BOTTOM,
+                       MotionToast.LONG_DURATION,
+                       getFont(this,R.font.helvetica_regular))
+
+// Directly takes the user to the setting for the respective request
+               val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+               startActivity(intent)
+
+           }else{
+               Dexter.withActivity(this).withPermissions(permission.ACCESS_COARSE_LOCATION,
+                                                          permission.ACCESS_FINE_LOCATION
+                             ).withListener(object : MultiplePermissionsListener{
+                   override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                       if(report!!.areAllPermissionsGranted()){
+                              requestLocationData()
+                       }
+                       if (report.isAnyPermissionPermanentlyDenied){
+                           darkToast(this@MainActivity,"INFO", "You have denied the Permission.",
+                                   MotionToast.TOAST_INFO,
+                                   MotionToast.GRAVITY_BOTTOM,
+                                   MotionToast.LONG_DURATION,
+                                   getFont(this@MainActivity,R.font.helvetica_regular))
+                       }
+                   }
+
+                   override fun onPermissionRationaleShouldBeShown(permission: MutableList<PermissionRequest>?, token: PermissionToken?) {
+                       showDialogForPermission()
+                   }
+
+               }).onSameThread().check()
+
+           }
+
+        }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData(){
+
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest , mLocationCallback,
+                Looper.myLooper()
+        )
     }
+
+    private val mLocationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("Current Latitude", "$latitude")
+
+            val longitude = mLastLocation.longitude
+            Log.i("Current Longitude", "$longitude")
+
+        }
+    }
+
+
+
+    fun showDialogForPermission(){
+        AlertDialog.Builder(this)
+                .setMessage("It looks likes you have turned off the permissions. Please turned it On by the Setting")
+                .setPositiveButton("GO TO SETTINGS"){ _,_  ->
+                    try {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package",packageName,null)
+                        intent.data = uri
+                        startActivity(intent)
+                    }catch (e: ActivityNotFoundException){
+                        e.printStackTrace()
+                    }
+
+                }.setNegativeButton("Cancel"){ dialog, _ ->
+                    dialog.dismiss()
+                    darkToast(this@MainActivity,"INFO", "You have denied the Permission.",
+                            MotionToast.TOAST_INFO,
+                            MotionToast.GRAVITY_BOTTOM,
+                            MotionToast.LONG_DURATION,
+                            getFont(this@MainActivity,R.font.helvetica_regular))
+                }.show()
+    }
+
+
+  private fun isLocationEnabled(): Boolean{
+
+        // This provides access to the system location services.
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+
+    private fun darkToast(context: MultiplePermissionsListener, title: String, message: String, gravityBottom: Int, longDuration: Long, font: Typeface?) {}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
